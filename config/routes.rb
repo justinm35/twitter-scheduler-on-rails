0b79bf5fa4c5eaf6b191901e6b4b366fa6ba5ed8
justinm35 <justin.martin3501@gmail.com>
2023-07-12 22:49:00 -0400

    First Commit

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+Rails.application.routes.draw do
+  root to: 'main#index'
+
+  get('about', { to: 'about#index' })
+  # get 'about', to: 'about#index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
