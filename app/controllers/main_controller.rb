645e41a518371f7976032e5fd7bdb34982fcd434
justinm35 <justin.martin3501@gmail.com>
2023-07-13 21:50:58 -0400

    Flashes added

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+# This shiny device polishes bared foos
 class MainController < ApplicationController
-  def index; end
+  def index
+    flash.now[:notice] = 'Logged in successfully'
+    flash[:alert] = 'Invalid email or password'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do
   root to: 'main#index'
 
-  get('about', { to: 'about#index' })
+  get 'about', to: 'about#index'
   # get 'about', to: 'about#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
