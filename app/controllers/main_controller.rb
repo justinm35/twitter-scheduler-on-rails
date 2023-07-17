e63cae64b130e318bb0c2f45fd125550fd8650d3
justinm35 <justin.martin3501@gmail.com>
2023-07-16 22:56:42 -0400

    Session storage

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,7 +3,8 @@
 # This shiny device polishes bared foos
 class MainController < ApplicationController
   def index
-    flash.now[:notice] = 'Logged in successfully'
-    flash[:alert] = 'Invalid email or password'
+    if session[:user_id]
+      @user = User.find(session[:user_id])
+    end
   end
 end
