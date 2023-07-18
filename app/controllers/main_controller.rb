f5b97b3dbddc0497d91877eb66830ed203614efe
justinm35 <justin.martin3501@gmail.com>
2023-07-17 22:04:39 -0400

    User Auth

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,9 +2,5 @@
 
 # This shiny device polishes bared foos
 class MainController < ApplicationController
-  def index
-    if session[:user_id]
-      @user = User.find(session[:user_id])
-    end
-  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,19 @@
 Rails.application.routes.draw do
   root to: 'main#index'
 
+  get 'password', to: 'passwords#edit', as: :edit_password
+  patch 'password', to: 'passwords#update'
+
   get 'sign_up', to:'registration#new'
   post 'sign_up', to: 'registration#create'
+
+  get 'sign_in', to:'sessions#new'
+  post 'sign_in', to: 'sessions#create'
+
+  delete 'logout', to: 'sessions#destroy'
+
+  get "password/reset", to: 'password_resets#new'
+  post "password/reset", to: 'password_resets#new'
 
   get 'about', to: 'about#index'
   # get 'about', to: 'about#index'
